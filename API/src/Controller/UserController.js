5a94c901141dfcdd3b198a07ebf362ccbd61a05e
ngocphuphamm <ngocphupham682001@gmail.com>
2022-05-20 22:01:03 +0700

    edituser

--- API/src/Controller/UserController.js
+++ API/src/Controller/UserController.js
@@ -1,33 +1,38 @@
 const User = require("../models/User");
-const jwt_decode = require('jwt-decode');
+const jwt_decode = require("jwt-decode");
 class UserController {
-    // [POST] /api/save_user_info
-    async saveInfoUser(req,res,next)
-    {
-        console.log(req.body);
-    }
-    // [GET] /api/logout
-    async logOut(req,res,next)
-    {
-        res.send({
-            success: true,
-        })
-    }
+  // [POST] /api/save_user_info
+  async saveInfoUser(req, res, next) {
+    const token = req.headers.authorization.split(" ")[1];
+    const idUser = jwt_decode(token)._id;
+    await User.updateOne(
+      { _id: idUser },
+      {
+        $set: req.body,
+      }
+    );
+    const user = await User.findById(idUser);
+    res.send({
+      success: true,
+      user: user,
+    });
+  }
+  // [GET] /api/logout
+  async logOut(req, res, next) {
+    res.send({
+      success: true,
+    });
+  }
 
-
-    //[GET] /api/showInfoUser
-    async showInfoUser(req,res,next)
-    {
-       const token = req.headers.authorization.split(" ")[1];
-       const idUser = jwt_decode(token)._id;
-       const user = await User.findById(idUser);
-        res.send({
-            success: true,
-            user : user
-        })
-    }
-
-
-    
+  //[GET] /api/showInfoUser
+  async showInfoUser(req, res, next) {
+    const token = req.headers.authorization.split(" ")[1];
+    const idUser = jwt_decode(token)._id;
+    const user = await User.findById(idUser);
+    res.send({
+      success: true,
+      user: user,
+    });
+  }
 }
 module.exports = new UserController();
